2ea0f4da1bf73d8f7b26e9cc69dc08db68637601
Ricardo <ricardogonzalezamora@gmail.com>
2019-03-29 18:14:53 -0600

    initialize injection with react

--- src/index.js
+++ src/index.js
@@ -1,18 +1,19 @@
-//    console.log('SUP');
-//    chrome.webNavigation.onCommitted.addListener(function(e) {
-//   if (hasHostSuffix(e.url, 'google.com') ||
-//       hasHostSuffix(e.url, 'google.com.au')) {
-//    console.log('SUP');
-//   }
-// });
 
+import { BaseCSS } from 'styled-bootstrap-grid';
+import React from 'react';
+import { render }from 'react-dom';
+
+import  { Main } from './components/Main';
 
 import { createBlockOverlay } from './js/inject';
 
 createBlockOverlay()
 .then( response => {
   if(response.success){
-    console.log('Successfull');
+    console.log('Render my react components...');
+    render(
+      <Main/>, document.getElementById('blockOverlay')
+    );
   }
 })
 .catch(err => {
